82af7c41cbf616547ae63667b75a41126cba7d89
tobiuo <kaisei0000000@gmail.com>
2025-08-13 01:01:32 +0900

    スタート時のワープ先座標保持変数をテレポート関数内に記述

--- src/main/java/mee/example/hiragana_learn/VillagerTalkEventListener.java
+++ src/main/java/mee/example/hiragana_learn/VillagerTalkEventListener.java
@@ -1,4 +1,25 @@
 package mee.example.hiragana_learn;
 
-public class VillagerTalkEventListener {
+import com.google.gson.JsonObject;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerInteractEvent;
+import java.util.ArrayList;
+
+public class VillagerTalkEventListener implements Listener {
+    private final Plugin plugin;
+    private final ArrayList<JsonObject> mission;
+
+    public VillagerTalkEventListener(JavaPlugin plugin, ArrayList<JsonObject> mission){
+        this.plugin = plugin;
+        this.mission = mission;
+    }
+
+    @EventHandler
+    private void VillagerTalkEvent(PlayerInteractEvent event){
+
+    }
+
 }
