cf99e5ce8e98d83d6694e580d9885780edd5536a
tobiuo <kaisei0000000@gmail.com>
2025-08-12 19:14:37 +0900

    最初のコミット

---
+++ src/main/java/mee/example/hiragana_learn/Hiragana_learn.java
@@ -0,0 +1,17 @@
+package mee.example.hiragana_learn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hiragana_learn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
